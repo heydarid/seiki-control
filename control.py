35a71662b7f7d40fc252866210b16a4a1ef89c80
unknown <dheydari@stanford.edu>
2021-07-27 01:34:48 -0700

    classes

--- control.py
+++ control.py
@@ -9,7 +9,22 @@ NTT-Mabuchi Group
 """
 
 import serial
+from enum import Enum
+from enum import IntEnum
 
+class Axis(IntEnum):
+    INPUT_X = 1
+    OUTPUT_X = 2
+    INPUT_Y = 3
+    OUTPUT_Y = 4
+    INPUT_Z = 5
+    OUTPUT_Z = 6
+
+class Seiki:
+    def __init__(self, com_port):
+        self.com_port = com_port
+        self.baud_rate = 9600
+    def goto_abs(axis, )
 
 
 com_port = '5'  # subject to change...
