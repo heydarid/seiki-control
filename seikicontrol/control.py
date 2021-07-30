1d16a941f4418b09700c3abc8ddd970d33b47330
David Heydari <dheydari@stanford.edu>
2021-07-29 19:03:54 -0700

    fix

--- seikicontrol/control.py
+++ seikicontrol/control.py
@@ -13,12 +13,12 @@ from enum import Enum
 from enum import IntEnum
 
 class Axis(IntEnum):
-    INPUT_X = 1
-    OUTPUT_X = 2
+    INPUT_Z = 1
+    OUTPUT_Z = 2
     INPUT_Y = 3
     OUTPUT_Y = 4
-    INPUT_Z = 5
-    OUTPUT_Z = 6
+    INPUT_X = 5
+    OUTPUT_X = 6
 """
 TODO:
     0. Recieve data properly
@@ -27,8 +27,8 @@ TODO:
     3. Maximize throughput function
 """
 class Seiki:
-    def __init__(self, com_port='5'):
-        self.com_port = com_port
+    def __init__(self, com_port=5):
+        self.com_port = 'COM' + str(com_port)
         self.baud_rate = 9600  # common
         self.ser = serial.Serial(self.com_port, self.baud_rate, timeout=0.5)
     def speed(self, axis, selection):
@@ -37,7 +37,7 @@ class Seiki:
         writedata = 'SELSP ' + str(selection)
         w_data = (writedata + '\r').encode('utf-8')
         self.ser.write(w_data)
-    def goto_abs(axis, pos):
+    def goto_abs(self, axis, pos):
         writedata = 'AXI' + str(axis) + ":GOABS " + str(int(pos))
         w_data = (writedata + '\r').encode('utf-8')
         self.ser.write(w_data)
