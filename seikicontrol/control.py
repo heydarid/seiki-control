5416fa74618c7f886cbbf94a4cc9848cac57e22d
David Heydari <dheydari@stanford.edu>
2021-11-20 22:35:48 -0800

    new tkinter gui program

--- seikicontrol/control.py
+++ seikicontrol/control.py
@@ -1,5 +1,5 @@
 """
-Project Name    : Python sample program
+Project Name    : Python root control program
 File Name       : control.py
 Purpose         : Controlling the Suruga-Seiki DS102 controller box for a 6-axis
                     motorized stage system.
@@ -9,9 +9,17 @@ Authors         : D. Heydari and M. Catuneanu
 NTT-Mabuchi Group
 """
 
+from os import device_encoding
 import serial
 from enum import Enum
 from enum import IntEnum
+
+"""
+Conventions:
+    Z: optic axis (propagation direction)
+    X: in plane of table
+    Y: out of plane of table
+"""
 
 class Axis(IntEnum):
     INPUT_Z = 1
@@ -20,13 +28,14 @@ class Axis(IntEnum):
     OUTPUT_Y = 4
     INPUT_X = 5
     OUTPUT_X = 6
-"""
-TODO:
-    0. Use the above enums for axis control
-    1. Write a function that jogs motor
-    2. Ensure that goto_abs WAITS until desired position is achieved
-    3. Maximize throughput function
-"""
+
+class Units(IntEnum):
+    PULSE = 0
+    UM = 1
+    MM = 2
+    DEG = 3
+    MRAD = 4
+
 class Seiki:
     def __init__(self, com_port=5):
         self.com_port = 'COM' + str(com_port)
@@ -51,7 +60,7 @@ class Seiki:
         self.serial_write_read(w_data)
 
     def goto_abs(self, axis, pos):
-        writedata = 'AXI' + str(axis) + ":GOABS " + str(int(pos))
+        writedata = 'AXI' + str(axis.real) + ":GOABS " + str(int(pos))
         w_data = (writedata + '\r').encode('utf-8')
         self.serial_write_read(w_data)
 
@@ -65,12 +74,16 @@ class Seiki:
         w_data = (writedata + '\r').encode('utf-8')
         self.serial_write_read(w_data)
 
-    def jog(self, axis, divisions):
-        pass
+    def jog(self, axis, data, dir='CW'):
+        writedata = 'AXI' + str(axis.real) + ":PULS " + str(int(data))
+        writedata += ':GO ' + str(dir) + ':DW'
+        w_data = (writedata + '\r').encode('utf-8')
+        self.serial_write_read(w_data)
 
-    def drive_direction(self):
-        # this needs to act like a toggle switch.
-        pass
+    def set_units(self, axis, unit):
+        writedata = 'AXI' + str(axis.real) + ':UNIT ' + str(unit.real)
+        w_data = (writedata + '\r').encode('utf-8')
+        self.serial_write_read(w_data)
 
     # Parameter query
     @property
@@ -80,16 +93,39 @@ class Seiki:
         self.serial_write_read(w_data)
 
     def _get_position(self, axis):
-        writedata = 'AXI' + str(axis) + ':POS?'
+        writedata = 'AXI' + str(axis.real) + ':POS?'
         w_data = (writedata + '\r').encode('utf-8')
         self.serial_write_read(w_data)
 
     def _get_speed(self, axis):
-        writedata = 'AXI' + str(axis) + 'SELSP?'
+        writedata = 'AXI' + str(axis.real) + ':SELSP?'
         w_data = (writedata + '\r').encode('utf-8')
         self.serial_write_read(w_data)
 
     def _get_units(self, axis):
-        writedata = 'AXI' + str(axis) + 'UNIT?'
+        writedata = 'AXI' + str(axis.real) + ':UNIT?'
         w_data = (writedata + '\r').encode('utf-8')
         self.serial_write_read(w_data)
+
+    def _get_driver_division(self, axis):
+        writedata = 'AXI' + str(axis.real) + ':DRDIV?'
+        w_data = (writedata + '\r').encode('utf-8')
+        self.serial_write_read(w_data)
+    
+    def _get_resolution(self, axis):
+        # This is the travel distance per pulse, a function
+        # of the driver division setting.
+        writedata = 'AXI' + str(axis.real) + ':RESOLUT?'
+        w_data = (writedata + '\r').encode('utf-8')
+        self.serial_write_read(w_data)
+    
+    def _get_pulse_setting(self, axis):
+        writedata = 'AXI' + str(axis.real) + ':PULS?'
+        w_data = (writedata + '\r').encode('utf-8')
+        self.serial_write_read(w_data)
+    
+    def _verify_home(self, axis):  # (0), 1: (un)detected
+        writedata = 'AXI' + str(axis.real) + ':HOME?'
+        w_data = (writedata + '\r').encode('utf-8')
+        self.serial_write_read(w_data)
+    
---
+++ seikicontrol/gui.py
@@ -0,0 +1,15 @@
+"""
+Project Name    : Python GUI operation program
+File Name       : control.py
+Purpose         : Controlling the Suruga-Seiki DS102 controller box for a 6-axis
+                    motorized stage system.
+Creation Date   : 2021-Nov-11
+Authors         : D. Heydari
+
+NTT-Mabuchi Group
+"""
+
+import serial
+import serial.tools.list_ports
+import tkinter
+
