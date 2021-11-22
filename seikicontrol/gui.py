9da4132772ab5b1e77719940ce02f6cbd7970530
David Heydari <dheydari@stanford.edu>
2021-11-21 23:40:12 -0800

    gui updates

--- seikicontrol/gui.py
+++ seikicontrol/gui.py
@@ -8,22 +8,27 @@ Authors         : D. Heydari
 
 NTT-Mabuchi Group
 """
+from math import exp
 from tkinter import *
 from control import Seiki
 from seikicontrol.control import Axis
+from tkinter import ttk
+
+# TODO:
+# 1. Units
+# 2. Make sure updating of table actually happend AFTER stage stopped moving
+# 3. emergency stop button
 
 # main
 if __name__ == '__main__':
     root = Tk()
     root.title(u'SURUGA-SEIKI STAGE CONTROL PROGRAM')
-    root.geometry('1000x600')
+    root.geometry('500x500')
     Stage = Seiki()
 
     # Axis selection
-    axis_frame = Frame(root)
-    axis_frame.pack(fill="x")
-    axis_label = Label(axis_frame, text=u'Axis control:  ', font='Helvetica 15')
-    axis_label.pack(side=LEFT)
+    axis_frame = LabelFrame(root, text="Axis",  font='Helvetica 13', bg='lemon chiffon')
+    axis_frame.pack(fill="both")
     axisChoice = IntVar()
     for axis in Axis:
         btn = Radiobutton(axis_frame, text=axis.name, activebackground="red", variable=axisChoice, value=axis.value,
@@ -32,33 +37,39 @@ if __name__ == '__main__':
         btn.pack(side=LEFT, padx=4, pady=4)
 
     # Speed selection
-    speed_frame = Frame(root)
-    speed_frame.pack(fill='x')
-    speed_label = Label(speed_frame, text=u'Speed setting: ', font='Helvetica 15')
-    speed_label.pack(side=LEFT)
+    speed_frame = LabelFrame(root, text="Speed",  font='Helvetica 13', bg='azure')
+    speed_frame.pack(fill='both')
     speedSet = IntVar()
     for speed in range(10):
         btnSpeed = Radiobutton(speed_frame, text=str(speed), activebackground="red", variable=speedSet, value=speed, 
                                 indicatoron='False', borderwidth=3, font='Helvetica 10')
         btnSpeed.pack(side=LEFT, padx=4, pady=4)
+    speed_set_button = Button(speed_frame, text='SET')
+    speed_set_button.pack(side=RIGHT)
+    def speed_set_function(action):
+        Stage.set_speed(axisChoice.get(), speedSet.get())
+        update_table(axisChoice.get(), action="SPEED")
+    speed_set_button.bind("<Button-1>", speed_set_function)
 
-    parent_left = Frame(root, bg='yellow')
-    parent_left.pack(side=LEFT, expand=False, fill=BOTH)
-    # Absolute position
-    abs_pos_frame = Frame(parent_left)
-    abs_pos_frame.pack(side=LEFT, expand=False, fill=BOTH)
-    pos_label = Label(abs_pos_frame, text=u'POS', font='Helvetica 14')
-    pos_label.pack(side=TOP)
+    # control frame
+    control_frame = LabelFrame(root, text="Motion",  font='Helvetica 13', bg='seashell2')
+    control_frame.pack(fill=BOTH)
+    ## Absolute position
+    abs_pos_frame = Frame(control_frame, bg='seashell2', borderwidth=6, relief=RIDGE)
+    abs_pos_frame.pack(side=LEFT, padx=15)
+    pos_button = Button(abs_pos_frame, text=u'To Pos', font='Helvetica 12 italic')
+    pos_button.pack(side=BOTTOM)
     position_desired = Entry(abs_pos_frame, width=15)
-    position_desired.pack()
+    position_desired.pack(side=LEFT)
 
-    # Jogging with arrows (right is CW, left is CCW)
-    jog_frame = Frame(parent_left)
-    jog_frame.pack(side=LEFT, expand=False, fill=BOTH)
-    jog_label = Label(jog_frame, text=u'JOG', font='Helvetica 14')
-    jog_label.pack(side=TOP)
+    ## Jogging
+    jog_frame = Frame(control_frame, bg='seashell2', borderwidth=6, relief=RIDGE)
+    jog_frame.pack(side=LEFT, padx=15)
 
-    direction = IntVar()
+    jog_button = Button(jog_frame, text=u'JOG', font='Helvetica 12 italic', wraplength=1)
+    jog_button.pack(side=RIGHT)
+
+    direction = StringVar()
     btnCW = Radiobutton(jog_frame, text='CW', activebackground="red", variable=direction, value='CW',
                             indicatoron='False', borderwidth=3, font='Helvetica 10')
     btnCW.pack(side=TOP, padx=4, pady=4)
@@ -67,58 +78,15 @@ if __name__ == '__main__':
     btnCCW = Radiobutton(jog_frame, text='CCW', activebackground="red", variable=direction, value='CCW',
                             indicatoron='False', borderwidth=3, font='Helvetica 10')
     btnCCW.pack(side=TOP, padx=4, pady=4)
-    ### NEED A JOG BUTTON
-    
-
-    # Status table
-    status_frame = Frame(root, bg = 'lime')
-    status_frame.pack(side=LEFT, expand=False, fill=BOTH)
-
-    status_label = Label(status_frame, text=u'STATUS', font='Helvetica 14', wraplength=1)
-    status_label.pack(side=LEFT, fill='x')
-
-    status_grid_frame = Frame(status_frame)
-    status_grid_frame.pack(side=LEFT, fill='x')
-    cells = {}
-    for row in range(7):
-        for column in range(5):
-            cell = Label(status_grid_frame, relief=RIDGE)
-            cell.grid(row=row, column=column, sticky="ew")
-            cells[(row, column)] = cell
-    ## labels
-    cells[(0,0)].configure(background='light gray')
-    cells[(0,0)].configure(text='AXIS')
-    for row in range(1,7):
-        cells[(row, 0)].configure(text=[axis.name for axis in Axis][row-1])
-    cells[(0,1)].configure(text='Drv Division')
-    cells[(0,2)].configure(text='Resol')
-    cells[(0,3)].configure(text='Abs Position')
-    cells[(0,4)].configure(text='Speed Set')
-    ## table data
-    def update_table(axis):
-        read_drvdiv = str(Stage._get_driver_division(axis).strip().decode("utf-8"))
-        read_position = str(Stage._get_position(axis).strip().decode("utf-8"))
-        read_resolution = str(Stage._get_resolution(axis).strip().decode("utf-8"))
-        read_speed = str(Stage._get_speed(axis).strip().decode("utf-8"))
-        cells[(axis, 1)].configure(text=read_drvdiv, background='white')
-        cells[(axis, 2)].configure(text=read_resolution, background='white')
-        cells[(axis, 3)].configure(text=read_position, background='white')
-        cells[(axis, 4)].configure(text=read_speed, background='white')
-        [root.after(10, lambda: cells[(axis, i)].configure(background='red')) for i in range(1,5)]
-        [root.after(2000, lambda: cells[(axis, i)].configure(background='white')) for i in range(1,5)]
-    # [update_table(axis.value) for axis in Axis]  # Initialize
-    
-
-    # Execute action
-    exec_frame = Frame(root, borderwidth=4)
-    exec_frame.pack(padx=10, pady=10)
-    def options():
-        Stage.set_speed(axisChoice.get(), speedSet.get())
-        update_table(axisChoice.get())
+    ## Button functions
+    def jog_function(action):
+        Stage.jog(axisChoice.get(), jog_amount.get(), direction.get())
+        update_table(axisChoice.get(), action="MOVE")
+    jog_button.bind("<Button-1>", jog_function)
+    def go_to_position_function(action):
         Stage.goto_abs(axisChoice.get(), position_desired.get())
-        update_table(axisChoice.get())
-    execButton = Button(exec_frame, text="GO & REFRESH",  font='Helvetica 10', width=15, height=2, command=options)
-    execButton.pack()
+        update_table(axisChoice.get(), action="MOVE")
+    pos_button.bind("<Button-1>", go_to_position_function)
 
     # emergency stop  (WIP)
     xOct, yOct = 25, 25
@@ -130,16 +98,66 @@ if __name__ == '__main__':
         points.append(x0)
         points.append(y0)
     points = tuple(points)
-    stop = Canvas(root, width=50, height=50, highlightthickness=0, relief='raised')
+    stop = Canvas(control_frame, bg='seashell2', width=50, height=50, highlightthickness=0, relief='raised')
     Stop = stop.create_polygon(points, smooth=False, fill='red', outline='black')
     stop.create_text(xOct, yOct, text='STOP!', fill="white", font=('Helvetica 10 bold'))
-    stop.place(anchor='se', relx=1.0, rely=1.0)
+    stop.pack(fill = "both", side = RIGHT)
     def custom_stop(action):
         if int(Stage._verify_all_moving().strip().decode("utf-8")) != 0:
             Stage.slow_stop()
         stop.itemconfig(Stop, fill='yellow')
         stop.itemconfig(Stop, fill='purple')
-
     stop.bind("<ButtonPress-1>", custom_stop)
+
+
+    # Status table
+    status_frame = LabelFrame(root, text=u'Stage Status', font='Helvetica 13', bg='light yellow')
+    status_frame.pack(fill=BOTH)
+
+    status_grid_frame = Frame(status_frame)
+    status_grid_frame.pack(side=LEFT)
+    cells = {}
+    for row in range(7):
+        for column in range(5):
+            cell = Label(status_grid_frame, relief=RIDGE)
+            cell.grid(row=row, column=column, sticky="ew")
+            cells[(row, column)] = cell
+    ## labels
+    cells[(0,0)].configure(background='light gray')
+    cells[(0,0)].configure(text='AXIS')
+    for row in range(1,7):
+        cells[(row, 0)].configure(text=[axis.name for axis in Axis][row-1])
+    cells[(0,1)].configure(text='Driver Division', font='Helvetica 10')
+    cells[(0,2)].configure(text='Resolution', font='Helvetica 10')
+    cells[(0,3)].configure(text='Abs Position', font='Helvetica 10')
+    cells[(0,4)].configure(text='Speed Set', font='Helvetica 10')
+    ## table data
+    def update_table(axis, action):
+        while int(Stage._verify_all_moving().strip().decode("utf-8")) != 0:
+            continue
+        cells[(axis, 3)].configure(background='white')
+        read_drvdiv = str(Stage._get_driver_division(axis).strip().decode("utf-8"))
+        cells[(axis, 1)].configure(text=read_drvdiv, font='Helvetica 10', background='white')
+
+        read_resolution = str(Stage._get_resolution(axis).strip().decode("utf-8"))
+        cells[(axis, 2)].configure(text=read_resolution, font='Helvetica 10', background='white')
+        i = 4
+        read_speed = str(Stage._get_speed(axis).strip().decode("utf-8"))
+        cells[(axis, i)].configure(text=read_speed, font='Helvetica 10', background='white')
+        root.after(10, lambda: cells[(axis, i)].configure(background='spring green'))
+        root.after(2000, lambda: cells[(axis, i)].configure(background='white'))
+
+        if action == 'MOVE':
+            i = 3
+            read_position = str(Stage._get_position(axis).strip().decode("utf-8"))
+            cells[(axis, 3)].configure(text=read_position, font='Helvetica 10', background='white')
+            root.after(10, lambda: cells[(axis, i)].configure(background='spring green'))
+            root.after(2000, lambda: cells[(axis, i)].configure(background='white'))
+
+    # copyright label
+    copyright = Button(root, text=u'© 2021 NTT-Mabuchi Research Co. LTD.', relief=SUNKEN, font='Helvetica 8')
+    copyright.pack(side=BOTTOM, fill=X)
+
+
 
     root.mainloop()
